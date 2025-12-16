cf86d9aa14ca5d64252d7c58cdda4714e52bcbdc
Eman Ayyad <eman_khalifa_ayyad@hotmail.com>
2025-12-16 09:34:04 +0200

    swagger implementation for authenticate and signup routes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Sign Up Api')
+    .setDescription(
+      'API to the sign up app including, authenticate, sign-up and get users',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/models/sign-up.model.ts
+++ src/models/sign-up.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -8,26 +9,38 @@ import {
 import { passwordRegex } from 'src/utils/globals';
 
 export class CreateUserModel {
+  @ApiProperty({
+    description: 'user name',
+    minLength: 3,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   name: string;
 
+  @ApiProperty({
+    description:
+      'user password, should contain one letter, one number and one special char. not less that 8 chars',
+    minLength: 8,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
   @Matches(passwordRegex)
   password: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 }
 
 export class AuthenticateUserModel {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 }
